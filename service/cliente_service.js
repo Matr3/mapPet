b4b6bbbae210bf1adeab7b9543674c8c091b0f69
Matr3 <tremari.marce@gmail.com>
2022-10-02 15:31:37 -0300

    agregando scripts login

--- service/cliente_service.js
+++ service/cliente_service.js
@@ -1,9 +1,12 @@
 //Fetch API
 const url = "https://62faaedbffd7197707f152a5.mockapi.io/pets";
+const url1 = "https://62faaedbffd7197707f152a5.mockapi.io/user";
 
 const listaBuscados = () => fetch("https://62faaedbffd7197707f152a5.mockapi.io/pets").then((respuesta) => respuesta.json()).catch((error) => error);
 
 const listaEncontrados = () => fetch("https://62faaedbffd7197707f152a5.mockapi.io/pest_encontrados").then((respuesta) => respuesta.json()).catch((error) => error);
+
+const listaUsuarios = () => fetch("https://62faaedbffd7197707f152a5.mockapi.io/user").then((respuesta) => respuesta.json()).catch((error) => error);
 
 const crearCliente = (imagen, categoria, nombre_prod, precio_prod, descripcion_prod) => {
     console.log(imagen, categoria, nombre_prod, precio_prod, descripcion_prod)
@@ -16,6 +19,22 @@ const crearCliente = (imagen, categoria, nombre_prod, precio_prod, descripcion_p
             nombre_prod,
             precio_prod,
             descripcion_prod
+      
+        })
+    })
+}
+
+const crearUsuario = (email, nombre, apellido, nombreCompleto, imagen) => {
+    console.log(email, nombre, apellido, nombreCompleto, imagen)
+    return fetch((`${url1}`),{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({
+            email,
+            nombre,
+            apellido,
+            nombreCompleto,
+            imagen
       
         })
     })
@@ -57,4 +76,7 @@ export const listaServices = {
     crearCliente,
     actualizarProducto,
     detalleProducto
+}
+export const listaClientes = {
+    crearUsuario
 }
