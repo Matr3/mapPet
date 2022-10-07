016251013c6aa7b2951d65949287efa99485c099
Matr3 <tremari.marce@gmail.com>
2022-10-07 17:12:38 -0300

    Capturar lat lng en el formulario

--- js/buscados.js
+++ js/buscados.js
@@ -1,20 +1,17 @@
 import { listaServices } from "../service/cliente_service.js";
 //backticks
+
 const crearNuevaLinea = (imagen, raza, id) => {
     const linea = document.createElement("div");
-    
     const contenido = `
     <div class="mascotas">
     <a class="link_mascotas" href="./detalle_pets.html?id=${id}&categoria=${raza}">
-    <div class="wh_box">
     <div class="bng_box">
     <img class="img" src="${imagen}" alt="${raza}">
-    </div>
     </div>
     <div>
       <ul class="detalles_tarjeta">
         <li class="descripcion">${raza}</li>
-        
         <li>Mas detalles</li>
       </ul>   
     </div>
@@ -26,15 +23,16 @@ const crearNuevaLinea = (imagen, raza, id) => {
     return linea;
   };
 
+
   
 const div = document.querySelector("[data-buscados]");
 let cont_b = 0;
 listaServices
   .listaBuscados()
   .then((data) => {
-    data.forEach(({ imagen, raza, descripcion,  id}) => {
+    data.forEach(({ imagen, raza, id}) => {
       if (cont_b < 6){
-        const nuevaLinea = crearNuevaLinea(imagen, raza, descripcion, id);
+        const nuevaLinea = crearNuevaLinea(imagen, raza, id);
         div.appendChild(nuevaLinea);
         cont_b++;
       }
